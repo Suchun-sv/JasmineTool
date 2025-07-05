2ca9a1c199af6dc764d30df8789280387ee62351
suchunsv <suchunsv@outlook.com>
2025-07-05 16:46:36 +0100

    update version 0.1.1

--- src/jasminetool/version.py
+++ src/jasminetool/version.py
@@ -5,10 +5,11 @@
 Version information for JasmineTool
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __version_info__ = tuple(int(x) for x in __version__.split("."))
 
 # Version history
 VERSION_HISTORY = [
+    "0.1.1 - Fix DVC cache directory setup",
     "0.1.0 - Initial release with multi-GPU/multi-host orchestration support",
 ]
